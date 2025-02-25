5eac89918d891945d9b6ed4a30156a42adb195b3
artur <grit4in0225@icloud.com>
2025-02-25 16:06:29 +0100

    finish todo-list

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Simple calculator
+# ToDo List
--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello, world!');
+const mainContainer = document.getElementById('main-container');
+const inputContainer = document.getElementById('input-container');
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const todoList = document.getElementById('todo-list');
+
+button.addEventListener('click', () => {
+    if (input.value === '') {
+        return;
+    } else {
+        const toDo = document.createElement('li');
+        toDo.classList.add('to-do');
+        toDo.textContent = input.value;
+        input.value = '';
+
+        const closeBtn = document.createElement('button');
+        closeBtn.classList.add('close-btn');
+        closeBtn.textContent = '❌';
+
+        closeBtn.addEventListener('click', () => {
+            toDo.remove();
+        });
+
+        toDo.appendChild(closeBtn);
+
+        todoList.appendChild(toDo);
+    }
+});
