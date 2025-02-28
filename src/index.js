f7c87b66cf40a15f766bb37bdd11edd2db96173a
artur <grit4in0225@icloud.com>
2025-02-28 12:52:31 +0100

    added local storage

--- src/index.js
+++ src/index.js
@@ -4,45 +4,57 @@ const input = document.getElementById('input');
 const button = document.getElementById('button');
 const todoList = document.getElementById('todo-list');
 
+let data = JSON.parse(localStorage.getItem('myData')) || [];
+
+function getStorage() {
+    localStorage.setItem('myData', JSON.stringify(data));
+}
+
 button.addEventListener('click', () => {
     if (input.value === '') {
         return;
     } else {
-        const toDo = document.createElement('li');
-        toDo.classList.add('to-do');
-
-        const toDoText = document.createElement('p');
-        toDoText.classList.add('to-do-text');
-        toDoText.textContent = input.value;
+        data.push(input.value);
+        updateList();
         input.value = '';
-
-        const controlContainer = document.createElement('div');
-        controlContainer.classList.add('control-container');
-
-        const editBtn = document.createElement('button');
-        editBtn.classList.add('control-btn');
-        editBtn.textContent = '✏️';
-
-        editBtn.addEventListener('click', () => {
-            const newText = prompt('Change your text:', toDoText.textContent);
-            if (newText !== null) {
-                toDoText.textContent = newText;
-            }
-        });
-
-        const closeBtn = document.createElement('button');
-        closeBtn.classList.add('control-btn');
-        closeBtn.textContent = '❌';
-
-        closeBtn.addEventListener('click', () => {
-            toDo.remove();
-        });
-
-        controlContainer.appendChild(editBtn);
-        controlContainer.appendChild(closeBtn);
-        toDo.appendChild(toDoText);
-        toDo.appendChild(controlContainer);
-
-        todoList.appendChild(toDo);
+        return;
     }
 });
+
+function updateList() {
+    todoList.innerHTML = createElement();
+}
+
+function createElement() {
+    getStorage();
+
+    return data
+        .map(
+            (toDo, i) =>
+                `<li class='to-do'>
+            <p class='to-do-text'>${toDo}</p>
+            <div class='control-container'>
+                <button class='control-btn' click onClick="getEdit(${i})">✏️</button>
+                <button class='control-btn' click onClick="getDelete(${i})">❌</button>
+            </div>
+        </li>`
+        )
+        .join('');
+}
+
+function getEdit(i) {
+    const newText = prompt('Change your text:', data[i]);
+    if (newText !== null) {
+        data[i] = newText;
+        getStorage();
+        updateList();
+    }
+}
+
+function getDelete(i) {
+    data.splice(i, 1);
+    getStorage();
+    updateList();
+}
+
+updateList();
